61d55af2a86cae14848b7f045afd82797cf423bd
Kris Burton <burton.kris@outlook.com>
2018-09-11 13:50:22 +0100

    HTML ID's for JS .innerHTML || JS RPS logic draft (needs tweaking)

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -2,27 +2,27 @@
 //start date - 27/07/2018
 //project - 1
 
-const items = {
+const choice = {
   0: 'rock',
   1: 'paper',
   2: 'scissors',
 };
-function gameRules(x, y) {
-  items[0] > items[1]
-  items[1] > items[2]
-  items[2] > items[0]
-};
-function p1() {
-  p1 = Math.floor(Math.random()*3) 
-  return p1
-};
-function p2() {
-  p2 = Math.floor(Math.random()*3) 
-  return p2
+p1 = Math.floor(Math.random()*3) 
+p2 = Math.floor(Math.random()*3)
+
+
+function gameRules(p1, p2) {
+  document.getElementById('p1Result').innerHTML = "Player one: " + choice[p1];
+  document.getElementById('p2Result').innerHTML = "Player two: " + choice[p2];
+  if (choice[p1] === choice[p2]) {
+    document.getElementById('gameResult').innerHTML = "Its a Draw! " + choice[p1] + " " + choice[p2] }
+  else if (choice[p1] === choice[0] && choice[p2] === choice[2]) {
+    document.getElementById('gameResult').innerHTML = "Rock bets Scissors, player one won"}
+  else if (choice[p1] > choice[p1]) {
+    document.getElementById('gameResult').innerHTML = "Player one won with " + choice[p1]}
+  else { document.getElementById('gameResult').innerHTML = "Player 2 won with " + choice[p2] + " *" + choice[p1] + "*"}
 };
 
-//document.getElementById("p1Result").innerHTML = items[p1()];
-//document.getElementById("p2Result").innerHTML = items[p2()];
 
-document.write(p1());
+console.log(gameRules(p1, p2))
 
