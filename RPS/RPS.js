62c8cd39f0d186e190d85d5f542b979f6f341a4b
Kris Burton <burton.kris@outlook.com>
2018-09-11 13:53:00 +0100

    format of logic block for easier reading

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -14,13 +14,17 @@ p2 = Math.floor(Math.random()*3)
 function gameRules(p1, p2) {
   document.getElementById('p1Result').innerHTML = "Player one: " + choice[p1];
   document.getElementById('p2Result').innerHTML = "Player two: " + choice[p2];
+
   if (choice[p1] === choice[p2]) {
     document.getElementById('gameResult').innerHTML = "Its a Draw! " + choice[p1] + " " + choice[p2] }
+
   else if (choice[p1] === choice[0] && choice[p2] === choice[2]) {
     document.getElementById('gameResult').innerHTML = "Rock bets Scissors, player one won"}
+
   else if (choice[p1] > choice[p1]) {
     document.getElementById('gameResult').innerHTML = "Player one won with " + choice[p1]}
-  else { document.getElementById('gameResult').innerHTML = "Player 2 won with " + choice[p2] + " *" + choice[p1] + "*"}
+
+  else { document.getElementById('gameResult').innerHTML = "Player 2 won with " + choice[p2]}
 };
 
 
