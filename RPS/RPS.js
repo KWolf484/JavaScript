9652c76db79ba8cf681e4acbadcf18d1aab3df70
Kris Burton <burton.kris@outlook.com>
2018-09-14 14:11:25 +0100

    RPS Logic

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -3,28 +3,38 @@
 //project - 1
 
 const choice = {
-  0: 'rock',
-  1: 'paper',
-  2: 'scissors',
+  0: 'Rock',
+  1: 'Paper',
+  2: 'Scissors',
 };
+// random number gen for RPS choice, two players. Poss AI func
 p1 = Math.floor(Math.random()*3) 
 p2 = Math.floor(Math.random()*3)
+// test for draw output
+// p1 = 0
+// p2 = 0
 
 
 function gameRules(p1, p2) {
-  document.getElementById('p1Result').innerHTML = "Player one: " + choice[p1];
-  document.getElementById('p2Result').innerHTML = "Player two: " + choice[p2];
+  document.getElementById('p1Result').innerHTML = "Player One: " + choice[p1];
+  document.getElementById('p2Result').innerHTML = "Player Two: " + choice[p2];
 
   if (choice[p1] === choice[p2]) {
-    document.getElementById('gameResult').innerHTML = "Its a Draw! " + choice[p1] + " " + choice[p2] }
+    document.getElementById('gameResult').innerHTML = "It\'s a Draw! " + "Player One Choose: " + choice[p1] + "Player Two Choose: " + choice[p2]
+  }
 
-  else if (choice[p1] === choice[0] && choice[p2] === choice[2]) {
-    document.getElementById('gameResult').innerHTML = "Rock bets Scissors, player one won"}
+  else if (choice[p1] === choice[0] && choice[p2] === choice[1] || choice[p2]
+    === choice[0] && choice[p1] === choice[1]) {
+    document.getElementById('gameResult').innerHTML = choice[1] + " Bets " + choice[0]
+  }
 
-  else if (choice[p1] > choice[p1]) {
-    document.getElementById('gameResult').innerHTML = "Player one won with " + choice[p1]}
+  else if (choice[p1] === choice[1] && choice[p2] === choice[2] || choice[p1] === choice[2a]) {
 
-  else { document.getElementById('gameResult').innerHTML = "Player 2 won with " + choice[p2]}
+  }
+
+
+  
+
 };
 
 
