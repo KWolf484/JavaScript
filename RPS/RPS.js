ac6c064541f587e16591b588d86f2f897ed6aaed
Kris Burton <burton.kris@outlook.com>
2018-09-18 11:05:53 +0100

    start on button, html formChildbutton

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -7,9 +7,11 @@ const choice = {
   1: 'Paper',
   2: 'Scissors',
 };
+// attempt at user input via buttons
+choice[0] = getElementById
 // random number gen for RPS choice, two players. Poss AI func
-p1 = Math.floor(Math.random()*3) 
-p2 = Math.floor(Math.random()*3)
+// p1 = Math.floor(Math.random()*3) 
+// p2 = Math.floor(Math.random()*3)
 // test for draw output
 // p1 = 0
 // p2 = 0
@@ -18,7 +20,6 @@ p2 = Math.floor(Math.random()*3)
 function gameRules(p1, p2) {
   document.getElementById('p1Result').innerHTML = "Player One: " + choice[p1];
   document.getElementById('p2Result').innerHTML = "Player Two: " + choice[p2];
-
   if (choice[p1] === choice[p2]) {
     document.getElementById('gameResult').innerHTML = "It\'s a Draw! <br />" + "Player One Chose: " + choice[p1] + "<br />Player Two Chose: " + choice[p2]
   }
