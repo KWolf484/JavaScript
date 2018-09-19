7917e6ef88a20c3dceec460c9e18e54a57bff732
Kris Burton <burton.kris@outlook.com>
2018-09-19 11:58:53 +0100

    recommit, version conflict

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -21,15 +21,11 @@ function gameRules(p1, p2) {
   document.getElementById('p1Result').innerHTML = "Player One: " + choice[p1];
   document.getElementById('p2Result').innerHTML = "Player Two: " + choice[p2];
   if (choice[p1] === choice[p2]) {
-<<<<<<< HEAD
-    document.getElementById('gameResult').innerHTML = "It\'s a Draw! <br />" + "Player One Chose: " + choice[p1] + "<br />Player Two Chose: " + choice[p2]
-=======
     document.getElementById('gameResult').innerHTML = "It\'s a Draw! " 
     + "<br />" 
     + "Player One Choose: " + choice[p1]
     + "<br />" 
     + "Player Two Choose: " + choice[p2]
->>>>>>> 38f36662dc1a045a2e04137e92c6f64c160f0586
   }
 
   else if (choice[p1] === choice[0] && choice[p2] === choice[1] || choice[p2]
@@ -37,12 +33,7 @@ function gameRules(p1, p2) {
     document.getElementById('gameResult').innerHTML = choice[1] + " Beats " + choice[0]
   }
 
-<<<<<<< HEAD
   else if (choice[p1] === choice[1] && choice[p2] === choice[2] || choice[p1] === choice[2]) { document.getElementById('gameResult').innerHTML = choice[2] +" Beats " + choice[1] 
-=======
-  else if (choice[p1] === choice[1] && choice[p2] === choice[2] || choice[p1] === choice[2] && choice[p2] === choice[1]) {
-
->>>>>>> 38f36662dc1a045a2e04137e92c6f64c160f0586
   }
 
 
